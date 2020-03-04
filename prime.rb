c362f8e99adafbcb5df77dc5ce69231a2a3d01d5
Christopher Chaison <cchaison0@gmail.com>
2020-03-04 23:47:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,10 @@
 def prime?(number)
-  (2..(number - 1)).each do |p|
-    return false if number % p == 0
+  if number > 0
+    (2..(number - 1)).each do |p|
+      return false if number % p == 0
+    end
+    true
+  else
+    false
   end
-  true
 end
